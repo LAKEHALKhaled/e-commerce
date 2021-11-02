52f77e24775d5beb3d34981c780049ab1237eb45
KhaledLakehal <khaled.lakehal93@gmail.com>
2021-11-02 13:31:30 -0400

    added pages Cart, Login, Product, ProductList and Register

---
+++ src/pages/Login.jsx
@@ -0,0 +1,74 @@
+
+import styled from "styled-components"
+
+const Container = styled.div`
+width :100vw;
+height:100vh;
+background:linear-gradient(rgba(255,255,255,0.5),rgba(255,255,255,0.5)), url("https://scontent-atl3-2.xx.fbcdn.net/v/t1.18169-9/1560581_1471132703166754_3486990106646306268_n.jpg?_nc_cat=106&ccb=1-5&_nc_sid=973b4a&_nc_ohc=Cv1SS-6aUHoAX9vz4HJ&_nc_oc=AQnw8WvgtDs8Ac5DDhfLdDpIXyOT1jKC29nwHtf5aHaPPFNjXuWoyx39eOb7NBXBhZBsJuo33y0PiAmskmKd4a1_&_nc_ht=scontent-atl3-2.xx&oh=52672d98fc5854b61da15b4464ecdde2&oe=61A68A81") center;
+background-size: cover;
+display: flex;
+align-items: center;
+justify-content:center;
+`
+
+const Wrapper = styled.div`
+width: 25%;
+padding: 20px;
+background-color: white;
+`
+
+const Title = styled.h1`
+font-size: 24px;
+font-weight: 300;
+`
+
+const Form = styled.form`
+display:flex;
+flex-direction: column;
+`
+
+
+
+const Input = styled.input`
+flex:1;
+min-width:40%;
+margin: 10px 0;
+padding: 10px;`
+
+
+const Button = styled.button`
+width:40%;
+border:none;
+padding: 15px 20px;
+background-color: teal;
+color: white;
+cursor: pointer;
+margin-bottom: 10px;
+`
+const Link = styled.a`
+margin: 10px 0px;
+font-size:12px;
+text-decoration: underline;
+cursor: pointer;
+`
+
+ const Login = () => {
+    return (
+        <Container>
+            <Container>
+            <Wrapper>
+                <Title>SIGN IN</Title>
+                <Form>
+                <Input placeholder = "username"/>
+                <Input placeholder = "password"/>
+                <Button>LOGIN</Button>
+                <Link>FORGET PASSWORD </Link>
+                <Link>CREATE NEW ACCOUNT </Link>
+                </Form>
+            </Wrapper>
+        </Container>
+        </Container>
+    )
+}
+
+export default Login
---
+++ src/pages/ProductList.jsx
@@ -0,0 +1,79 @@
+
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import Products from "../components/Products";
+
+const Container = styled.div``
+const Title = styled.h1`
+margin:20px;`
+
+const FilterContainer = styled.div`
+display:flex;
+justify-content:space-between;
+`
+const Filter = styled.div`
+margin:20px;`
+
+const FilterText = styled.span`
+font-size: 20px;
+font-weight: 600;
+margin-right:20px;
+`
+const Select = styled.select`
+padding: 10px;
+margin-right: 20px;
+`
+
+const Option = styled.option`
+`
+
+ const ProductList = () => {
+    return (
+
+        <Container>
+            <Navbar/>
+           <Announcement/>
+           <Title>Magic Bling</Title>
+           <FilterContainer>
+            <Filter>
+            <FilterText>Filter Product:</FilterText>
+            <Select>
+            <Option disabled selected>
+            Product
+            </Option>
+            <Option>Magic Bling</Option>
+            <Option>Star Glow</Option>
+            <Option>Combo deals</Option>
+            </Select>
+            <Select>
+            <Option disabled selected>
+            Size
+            </Option>
+            <Option>32oz</Option>
+            <Option>16oz</Option>
+            <Option>8oz</Option>
+            <Option>4oz</Option>
+            <Option>2oz</Option>
+            </Select>
+            </Filter>
+            <Filter>
+            <FilterText>Sort Product:</FilterText>
+            <Select>
+            <Option selected>
+            Newest
+            </Option>
+            <Option>Price(asc)</Option>
+            <Option>Price(desc)</Option>
+            </Select>
+            </Filter>
+           </FilterContainer>
+           <Products/>
+           <Newsletter/>
+           <Footer/>
+        </Container>
+    )
+}
+export default ProductList
